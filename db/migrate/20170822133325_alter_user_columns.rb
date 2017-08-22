81b26e0573b2db88773d82fcc08c3936ac7d5ef6
Paul Angland <pangland101@gmail.com>
2017-08-22 09:39:16 -0400

    Fix user migration, write session util methods

---
+++ db/migrate/20170822133325_alter_user_columns.rb
@@ -0,0 +1,7 @@
+class AlterUserColumns < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :users, :username
+    add_column :users, :first_name, :string, null: false
+    add_column :users, :last_name, :string, null: false
+  end
+end
