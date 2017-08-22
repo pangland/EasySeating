7ac1295b7c31cc1b0ec903d184cf95cf7530a098
Paul Angland <pangland101@gmail.com>
2017-08-22 09:22:22 -0400

    Made user and session controllers, user model, and user views

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,26 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_user_by_credentials(user_params[:username], user_params[:password])
+    puts('Hey Jude');
+    if @user
+      login!(@user)
+      render json: @user
+    else
+      errors = ['Invalid creds']
+      render json: errors, status: 401
+    end
+
+  end
+
+  def destroy
+    if current_user
+      logout!
+      if session[:session_token].nil?
+        render json: {}
+        return
+      end
+    end
+    render json: ["No user to log out"], status: 404
+  end
+
+end
