da0d01d0cf9a21082474b68e263a7462e894dfeb
Paul Angland <pangland101@gmail.com>
2017-08-23 23:46:05 -0400

    Make fetch and create ajax functions. Make corresponding actions.

---
+++ frontend/actions/restaurant_actions.js
@@ -0,0 +1,23 @@
+import * as APIUtil from '../util/restaurant_api_util';
+
+export const RECEIVE_ALL_RESTAURANTS = 'RECEIVE_ALL_RESTAURANTS';
+export const RECEIVE_SINGLE_RESTAURANT = 'RECEIVE_SINGLE_RESTAURANT';
+
+export const receiveAllRestaurants = (restaurants) => {
+  return {
+    type: RECEIVE_ALL_RESTAURANTS,
+    restaurants
+  };
+};
+
+export const receiveSingleRestaurant = (restaurant) => {
+  return {
+    type: RECEIVE_SINGLE_RESTAURANT,
+    restaurant
+  };
+};
+
+export const createRestaurant = restaurant => dispatch => {
+  return APIUtil.createRestaurant(restaurant).then(restaurant => (
+    dispatch(receiveSingleRestaurant(restaurant))));
+};
