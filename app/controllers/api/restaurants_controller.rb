55cee65966e67dd19789f7cbdf7c761130fddd1f
Paul Angland <Paul Angland>
2017-08-28 17:56:44 -0400

    Controller stuff broke it. Fix controller stuff

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -18,14 +18,14 @@ class Api::RestaurantsController < ApplicationController
     @restaurant.rating = 0
 
     if @restaurant.save
-      Slot.create(restaurant_id: @restaurant.id, seats: 2, time: '11:00')
-      time = @restaurant.time == '0' ? '11:00'.to_time : '7:30'.to_time
-      final_time = @restaurant.time == '0' ? '10:00 p.m.'.to_time : '9:00 p.m.'.to_time
-
-      until time == final_time
-        Slot.create(restaurant_id: @restaurant.id, seats: 2, time: time)
-        time += 30.minutes
-      end
+      # Slot.create(restaurant_id: @restaurant.id, seats: 2, time: '11:00')
+      # time = @restaurant.time == '0' ? '11:00'.to_time : '7:30'.to_time
+      # final_time = @restaurant.time == '0' ? '10:00 p.m.'.to_time : '9:00 p.m.'.to_time
+      #
+      # until time == final_time
+      #   Slot.create(restaurant_id: @restaurant.id, seats: 2, time: time)
+      #   time += 30.minutes
+      # end
 
       render 'api/restaurants/show'
     else
