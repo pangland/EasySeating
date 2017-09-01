596be8f2a6dab0cf4cfd95e0d1303824fca7880b
Paul Angland <pangland101@gmail.com>
2017-09-01 15:07:36 -0400

    Fixed login mishap

--- frontend/components/session_form.jsx
+++ frontend/components/session_form.jsx
@@ -81,7 +81,7 @@ class SessionForm extends React.Component {
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.loggedIn) {
-      this.props.history.push('/');
+      // this.props.history.push('/');
     }
   }
 
