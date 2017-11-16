2f4902247f319388feff6df447b3e3d9902ab0e2
Paul Angland <pangland101@gmail.com>
2017-11-15 19:50:09 -0500

    Made style correction to restaurant model

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -13,19 +13,10 @@ class Api::RestaurantsController < ApplicationController
     offset_selected = s_time.to_date - Slot.first.time.to_date  - (post_adjusted_date - preadjusted_date)
     offset_current = c_time.to_date - Slot.first.time.to_date + (s_time.to_date - c_time.to_date);
 
-    #
-
     if params[:data][:search].present?
-      # @restaurants = Restaurant.search_name(params[:data][:search])
-      # .includes(slots: :reservations)
       temp = Restaurant.search_name(params[:data][:search])
         .joins(:reservations)
-        .where('time > ?', c_time - offset_current.days)
-        .where('time >= ?', s_time - offset_selected.days - 1.hours)
-        .where('time <= ?', s_time - offset_selected.days + 1.hours)
-        .where('seats = ?', params[:data][:seats])
-        .where('user_id IS NULL')
-        .where('date = ?', params[:data][:date].to_date)
+        .search_conditions(params[:data])
         .includes(slots: :reservations)
 
       hash = Hash.new(true)
@@ -38,15 +29,8 @@ class Api::RestaurantsController < ApplicationController
       end
     else
       @restaurants = Restaurant.distinct.joins(:reservations)
-        .where('time > ?', c_time - offset_current.days)
-        .where('time >= ?', s_time - offset_selected.days - 1.hours)
-        .where('time <= ?', s_time - offset_selected.days + 1.hours)
-        .where('seats = ?', params[:data][:seats])
-        .where('user_id IS NULL')
-        .where('date = ?', params[:data][:date].to_date)
+        .search_conditions(params[:data])
     end
-    # restaurant_ids = @restaurants.pluck(:id)
-    # @restaurants = @restaurants.includes(slots: :reservations)
 
     @reservations = Reservation.where(slot_id: Slot
       .where('time > ?', c_time - offset_current.days)
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,6 @@
 class Restaurant < ApplicationRecord
   include PgSearch
-  pg_search_scope :search_name, :against => [:name, :cuisine]
-
+  pg_search_scope :search_name, against: %i[name cuisine]
 
   validates :name, :description, :rating, :price, :hours, :cuisine, presence: true
 
@@ -14,6 +13,24 @@ class Restaurant < ApplicationRecord
     self.where("similarity(name, ?) > 0.2", query)
         .order("similarity(name,
         #{ActiveRecord::Base.connection.quote(query)}) DESC").limit(10)
+  end
+
+  def self.search_conditions(data)
+    s_time = Time.parse("#{data[:time]} -0500")
+    preadjusted_date = s_time.to_date
+    s_time = s_time.getlocal('-00:00')
+    post_adjusted_date = s_time.to_date
+    s_time = Time.parse(data[:date] + " " + s_time.to_s[11..-1])
+    c_time = Time.now.getlocal('-00:00')
+    offset_current = c_time.to_date - Slot.first.time.to_date + (s_time.to_date - c_time.to_date)
+    offset_selected = s_time.to_date - Slot.first.time.to_date  - (post_adjusted_date - preadjusted_date)
+
+    self.where('time > ?', c_time - offset_current.days)
+      .where('time >= ?', s_time - offset_selected.days - 1.hours)
+      .where('time <= ?', s_time - offset_selected.days + 1.hours)
+      .where('seats = ?', data[:seats])
+      .where('user_id IS NULL')
+      .where('date = ?', data[:date].to_date)
   end
 
   def get_reservations(data)
