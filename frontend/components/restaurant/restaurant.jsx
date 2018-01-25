fdcd4a6e47220a9ee76c2da22ee371036b77d89b
Paul Angland <pangland101@gmail.com>
2018-01-24 23:59:24 -0500

    Remove debugger

--- frontend/components/restaurant/restaurant.jsx
+++ frontend/components/restaurant/restaurant.jsx
@@ -137,7 +137,6 @@ class Restaurant extends React.Component {
   }
 
   render() {
-    debugger;
     if (!this.props.restaurant) return null;
     const reviews = this.getAllReviews();
     const reviewSummary = this.getReviewSummary();
